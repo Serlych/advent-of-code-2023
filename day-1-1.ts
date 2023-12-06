0eab5697613c7cd197e82bb8720ab941264f12fd
Isaac Chavoya <joelchavoyam@gmail.com>
2023-12-05 23:11:51 -0600

    day 1.1 :)

---
+++ day-1-1.ts
@@ -0,0 +1,33 @@
+const file = Bun.file('./day-1-1-input.txt');
+const text = await file.text();
+const lines = text.split('\n');
+
+let results = [];
+
+for (const line of lines) {
+  const charArray = line.replace(/\D/g,'').split('');
+  
+  let firstDigit = null;
+  let secondDigit = null;
+  
+  if (charArray.length === 0) {
+    continue;
+  }
+  
+  if (charArray.length === 1) {
+    firstDigit = charArray[0];
+    secondDigit = charArray[0];
+  }
+  
+  if (charArray.length >= 2) {
+    firstDigit = charArray[0];
+    secondDigit = charArray[charArray.length - 1];
+  }
+  
+  let twoDigits = firstDigit + secondDigit;
+  twoDigits = parseInt(twoDigits);
+  results.push(twoDigits);
+}
+
+const sum = results.reduce((a, b) => a + b, 0);
+console.log(sum);
