93c77c5fac6dde6ecc4b9d1a16cd20fd361ccf45
Isaac Chavoya <joelchavoyam@gmail.com>
2023-12-10 22:44:37 -0500

    complete day 2.2 :)

---
+++ day-2-2.ts
@@ -0,0 +1,23 @@
+const file = Bun.file('./day-2.input.txt');
+const text = await file.text();
+const lines = text.split('\n');
+
+lines.pop();
+
+const powers = lines.map(line => {
+  const sets = line.split(':')[1].split(';');
+  const colors = ['red', 'green', 'blue'];
+  
+  const maxValues = colors.map(color => {
+    const cubes = sets
+      .flatMap(set => set.split(',')
+      .filter(cube => cube.includes(color))
+      .map(cube => parseInt(cube.replace(/\D/g, ''))));
+    return Math.max(...cubes);
+  });
+  
+  return maxValues.reduce((a, b) => a * b, 1);
+});
+
+const sum = powers.reduce((a, b) => a + b, 0);
+console.log(sum);
